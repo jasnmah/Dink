b5e3892c188957201101c5b471c24690ca720876
jasnmah <jasnmah@gmail.com>
2026-03-29 20:53:54 -0700

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://jdqpgbusqskuionownwo.supabase.co';
+const supabaseAnonKey = 'sb_publishable_YOURFULLKEYHERE';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
